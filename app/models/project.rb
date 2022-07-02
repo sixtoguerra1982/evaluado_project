ea0d6e4b9038efa0e38661645a910c446d5c599f
Felipe Guerra <sixto.guerra1982@gmail.com>
2022-07-01 21:17:21 -0400

    pendiente formulario y buscador

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,20 @@
 class Project < ApplicationRecord
+
+    validates :name, presence: true
+    validates :description, presence: true
+
+    enum state: [:propuesta , :en_progreso , :terminado]
+
+    def self.load
+        20.times do |i|
+            Project.create(
+                name: Faker::Company.name,
+                description: Faker::Company.catch_phrase,
+                date_begin: Time.now,
+                date_end: Time.now + 3.days
+            )
+        end
+
+    end
+
 end
