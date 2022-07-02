c8301f197937af777b3b147b605336b0670dc1b1
Felipe Guerra <sixto.guerra1982@gmail.com>
2022-07-02 10:28:14 -0400

    form_tag for project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,25 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def create
+
+    if params[:state] == 'propuesta'
+      state = 0
+    elsif params[:state] == 'en_progreso'
+      state = 1
+    else
+      state = 2
+    end
+
+    
+    @project = Project.create(
+      name: params[:name],
+      description: params[:description],
+      date_begin: params[:date_begin],
+      date_end: params[:date_end],
+      state: state
+    )
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'projects/index'
   root 'projects#index'
+  post 'projects' , to: 'projects#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
